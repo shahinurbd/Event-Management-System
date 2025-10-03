25dc449ba1c677bfc953b676650f2e65103fdb7f
Muhammad Shahinur <shahinurislam728@gmail.com>
2025-10-03 08:47:18 +0600

    domain changed

--- event_management/settings.py
+++ event_management/settings.py
@@ -200,7 +200,7 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # FRONTEND_URL = 'http://127.0.0.1:8000'
-FRONTEND_URL = 'https://event-management-system-psdd.onrender.com'
+FRONTEND_URL = 'https://colors-event.onrender.com'
 
 LOGIN_URL = '/users/sign-in/'
 LOGIN_REDIRECT_URL = '/events/hero/'
